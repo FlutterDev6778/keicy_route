7fd1bd3c930b2aafc84fe6d8179d4fd97a186071
FlutterDev <Keicy6778@gmail.com>
2020-09-25 14:20:29 +0100

    2020-09-25
    Signed-off-by: FlutterDev <Keicy6778@gmail.com>

--- lib/keicy_route.dart
+++ lib/keicy_route.dart
@@ -16,32 +16,32 @@ enum PageTransitionType {
 }
 
 class KeicyRoute extends PageRouteBuilder {
-  final Widget page;
-  final String routeName;
-  final Object arguments;
-  final PageTransitionType transitionType;
-  final Duration transitionDuration;
-  final bool opaque;
-  final Curve curve;
+  Widget page;
+  String routeName;
+  Object arguments;
+  PageTransitionType transitionType;
+  Duration transitionDuration;
+  bool opaque;
+  Curve curve;
 
   KeicyRoute(
     this.routeName,
     this.page, {
     this.arguments,
-    this.transitionType,
-    this.transitionDuration,
-    this.opaque,
-    this.curve,
+    this.transitionType = PageTransitionType.fade,
+    this.transitionDuration = const Duration(milliseconds: 300),
+    this.opaque = true,
+    this.curve = Curves.ease,
   }) : super(
           settings: RouteSettings(
             name: routeName,
             arguments: arguments,
           ),
           pageBuilder: (context, animation, secondaryAnimation) => page,
-          transitionDuration: transitionDuration ?? Duration(milliseconds: 300),
-          opaque: opaque ?? true,
+          transitionDuration: transitionDuration,
+          opaque: opaque,
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            var tween = Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: curve ?? Curves.ease));
+            var tween = Tween<double>(begin: 0, end: 1).chain(CurveTween(curve: curve));
             switch (transitionType) {
               case PageTransitionType.fade:
                 return FadeTransition(
